98299dd7220e6f66ea27389af6957e0867ef18a1
xmmwc <xmmwc@qq.com>
2015-01-07 21:00:28 +0800

    加了arrayAdapter

--- app/src/main/java/com/zizo/fx/documentviewer/FileListActivity.java
+++ app/src/main/java/com/zizo/fx/documentviewer/FileListActivity.java
@@ -1,6 +1,10 @@
 package com.zizo.fx.documentviewer;
 
+import android.app.ListFragment;
+import android.content.Context;
+import android.database.DataSetObserver;
 import android.os.Looper;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -9,10 +13,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import com.zizo.fx.filelistgetter.FileList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FileListActivity extends ActionBarActivity {
@@ -24,7 +36,7 @@ public class FileListActivity extends ActionBarActivity {
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new FileListFragment())
                     .commit();
         }
     }
@@ -52,30 +64,45 @@ public class FileListActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    public static class FileListFragment extends Fragment {
 
-        public PlaceholderFragment() {
-
+        private List<FileItem> getFileItems(JSONObject data){
+            List<FileItem> fileItems = new ArrayList<>();
+            try {
+                JSONArray ja = data.getJSONArray("FileInfo");
+                for (int i=0;i<ja.length();i++){
+                    JSONObject jo = ja.getJSONObject(i);
+                    String name = jo.getString("Name");
+                    String fileType = jo.getString("FileType");
+                    String filePath = jo.getString("FilePath");
+                    int fileSize = jo.getInt("FileSize");
+                    fileItems.add(new FileItem(name,fileType,filePath,fileSize));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return fileItems;
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             setRetainInstance(true);
-            View rootView = inflater.inflate(R.layout.fragment_file_list, container, false);
-            ListView lv = (ListView) rootView.findViewById(R.id.file_list);
+            final View rootView = inflater.inflate(R.layout.fragment_file_list, container, false);
+            final ListView lv = (ListView) rootView.findViewById(R.id.file_list);
 
             new FileList().get("http://192.168.1.64/api/fileinfo")
             .success(new FileList.OnSuccessEvent() {
                 @Override
                 public void success(JSONObject data) {
+                    final List<FileItem> fileItems = getFileItems(data);
                     new android.os.Handler(Looper.getMainLooper()).post(new Runnable() {
                         @Override
                         public void run() {
-                            //lv.setAdapter();
+                            ArrayAdapter<FileItem> arrayAdapter =
+                                    new ArrayAdapter<FileItem>(rootView.getContext(),android.R.layout.simple_list_item_1,fileItems);
+                            lv.setAdapter(arrayAdapter);
+
                         }
                     });
                 }
@@ -93,6 +120,20 @@ public class FileListActivity extends ActionBarActivity {
 //            });
 
             return rootView;
+        }
+    }
+
+    public static class FileItem{
+        public String Name;
+        public String FilePath;
+        public String FileType;
+        public int FileSize;
+
+        public FileItem(String name, String fileType, String filePath, int fileSize) {
+            Name = name;
+            FilePath = filePath;
+            FileType = fileType;
+            FileSize = fileSize;
         }
     }
 }
