22a089d16f1b79addd0af6b747b20c2c62afb84c
xmmwc <xmmwc@qq.com>
2015-01-07 20:09:03 +0800

    little

--- app/src/main/java/com/zizo/fx/documentviewer/FileListActivity.java
+++ app/src/main/java/com/zizo/fx/documentviewer/FileListActivity.java
@@ -1,10 +1,7 @@
 package com.zizo.fx.documentviewer;
 
-import android.content.Intent;
-import android.os.Environment;
 import android.os.Looper;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,17 +9,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.Button;
 import android.widget.ListView;
 
 import com.zizo.fx.filelistgetter.FileList;
-
-import net.sf.andpdf.pdfviewer.PdfViewerActivity;
-
 import org.json.JSONObject;
-
-import java.util.logging.Handler;
 
 
 public class FileListActivity extends ActionBarActivity {
